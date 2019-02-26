de9a47d3566c5d1fe6a1fbd79493c641253a9b03
sunpriya <sunpriya.bhatia@eduwaive.org>
2019-02-26 12:18:26 +0530

    GLT-5:Request response format of API updated

--- Desktop/genric_labelling_tool/image_api/views.py
+++ Desktop/genric_labelling_tool/image_api/views.py
@@ -15,6 +15,13 @@ import time
 class gltList(APIView):
     renderer_classes = (JSONRenderer, )
     def get(self,request):
+        print(request.data.keys())
+        from_date=request.data['from']
+        to_date=request.data['to']
+        if('numberofunlabelledimages' in request.data.keys()):
+            number=request.data['numberofunlabelledimages']
+        else:
+            number=10
         creds = ServiceAccountCredentials.from_json_keyfile_name('/home/sunpriya/Downloads/eduwaivecommon-01cac388cb6b.json')
         client_id = creds.service_account_email
         delimiter='/'
@@ -30,18 +37,23 @@ class gltList(APIView):
 
         blobs = bucket.list_blobs(prefix='Samples/')
         links=[]
-        print('Blobs:')
+        count=0
+        #print('Blobs:')
         for blob in blobs:
-            
-            sign_string='GET\n'+"\n"+"\n"+EXPIRATION+"\n"+"/eduwaivesgditium/"+blob.name
-            print(sign_string)
-            signature = creds.sign_blob(sign_string)[1]
-            encoded_signature = base64.b64encode(signature).decode('utf-8')\
-                .replace("+","%2B").replace("/","%2F").replace("=",'%3D')
-            base_url="https://storage.googleapis.com/eduwaivesgditium/"+blob.name+\
-                "?GoogleAccessId=" + GOOGLE_ACCESS_STORAGE_ID + "&Expires=" +\
-                     EXPIRATION + "&Signature=" + encoded_signature  
-            links.append(base_url)
+            date=str(blob.updated).split(" ")[0]
+            if(date>=from_date and date<=to_date):
+                sign_string='GET\n'+"\n"+"\n"+EXPIRATION+"\n"+"/eduwaivesgditium/"+blob.name
+                
+                signature = creds.sign_blob(sign_string)[1]
+                encoded_signature = base64.b64encode(signature).decode('utf-8')\
+                    .replace("+","%2B").replace("/","%2F").replace("=",'%3D')
+                base_url="https://storage.googleapis.com/eduwaivesgditium/"+blob.name+\
+                    "?GoogleAccessId=" + GOOGLE_ACCESS_STORAGE_ID + "&Expires=" +\
+                        EXPIRATION + "&Signature=" + encoded_signature  
+                links.append(base_url)
+                count+=1
+                if(count==number):
+                    break
         return Response(links)
         
 
